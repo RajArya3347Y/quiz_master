db738739e22cec96a78b3c9c60644301f910bf7f
RajArya3347Y <raajaaryandewangan@gmail.com>
2025-01-26 14:11:09 +0530

    Working

--- src/QuizApp.jsx
+++ src/QuizApp.jsx
@@ -5,7 +5,8 @@ function QuizApp() {
 
     const [score, setScore] = useState(0);
     const [data, setData] = useState(null);
-    let [answers, setAnswers] = useState(null);
+    const [answers, setAnswers] = useState(null);
+    const [index, setIndex] = useState(0);
 
     // fetching api on mount
 
@@ -15,13 +16,10 @@ function QuizApp() {
         const result = await res.json()
         setData(result)
 
-
-        const correctAns = result.results[0].correct_answer;
-        const incorrectAnswers = result.results[0].incorrect_answers;
+        const correctAns = result.results[index].correct_answer;
+        const incorrectAnswers = result.results[index].incorrect_answers;
         setAnswers(_.shuffle([...incorrectAnswers, correctAns]))
     }
-
-    console.log(data)
 
     useEffect(() => {
         fetchData()
@@ -34,6 +32,42 @@ function QuizApp() {
     }
 
 
+    function checkAnswer(ele) {
+        const answers = document.querySelectorAll(".answer")
+        if (ele.target.innerText === data.results[index].correct_answer) {
+            ele.target.style.backgroundColor = "green"
+            setScore(score + 1)
+        }
+        else {
+            ele.target.style.backgroundColor = "red"
+        }
+        answers.forEach((ans) => {
+            ans.style.cursor = "not-allowed"
+            ans.style.pointerEvents = 'none'
+        })
+        document.querySelector("#next-btn").hidden = false
+    }
+
+    function nextQuestion() {
+        console.log(index);
+        if(index >= 9){
+            const nextBtn = document.querySelector("#next-btn")
+            nextBtn.innerText = "Completed Quiz"
+        }else{
+            const indx = index+1
+            setIndex((i) => i+1);
+            const correctAns = data.results[indx].correct_answer;
+            const incorrectAnswers = data.results[indx].incorrect_answers;
+            setAnswers(_.shuffle([...incorrectAnswers, correctAns]))
+            const answers = document.querySelectorAll(".answer")
+            answers.forEach((ans) => {
+                ans.style.cssText = ''
+            })
+            document.querySelector("#next-btn").hidden = true
+        }
+    }
+
+
     return (
         <div className="main">
             <div className="head">
@@ -41,15 +75,18 @@ function QuizApp() {
                 <p>Score: {score}</p>
             </div>
             <hr />
-            <p id="question">{data === null ? " Loading..." : decode(data.results[0].question)}</p>
+            <p id="question">{data === null ? " Loading..." : decode(data.results[index].question)}</p>
             <div id="answers">
                 {answers &&
                     answers.map((val, i) => (
-                        <button key={i} className="answer">{val}</button>
+                        <button 
+                            key={i} 
+                            className="answer" 
+                            onClick={(target) => checkAnswer(target)}>{decode(val)}</button>
                     ))}
             </div>
             <div className="next">
-                <button id="next-btn">Next</button>
+                <button id="next-btn" onClick={nextQuestion} hidden>Next</button>
             </div>
         </div>
     )
