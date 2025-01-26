90eb7334c6bc319131ecc28e553b84c8e5719966
RajArya3347Y <raajaaryandewangan@gmail.com>
2025-01-26 10:38:40 +0530

    API is fetching correctly

--- src/QuizApp.jsx
+++ src/QuizApp.jsx
@@ -3,6 +3,22 @@ import { useState, useEffect, useRef } from "react";
 function QuizApp() {
 
     const [score, setScore] = useState(0);
+    const [data, setData] = useState(null);
+
+    // fetching api on mount
+
+    const fetchData = async () => {
+        const URL = "https://opentdb.com/api.php?amount=10";
+        const res = await fetch(URL);
+        setData(await res.json());
+    }
+    
+    console.log(data)
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
 
     return (
         <div className="main">
@@ -11,7 +27,7 @@ function QuizApp() {
                 <p>Score: {score}</p>
             </div>
             <hr />
-            <p id="question">1. Lorem ipsum dolor sit consectetur.</p>
+            <p id="question">{data === null ? " Loading..." : data.results[0].question}</p>
             <div id="answers">
 
             </div>
